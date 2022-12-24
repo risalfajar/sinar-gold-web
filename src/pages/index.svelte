094a2cc9c0dd53cc9f5718acf6a2f018eded4544
risalfajar <risalfajar@gmail.com>
2022-12-24 15:03:40 +0700

    Initial commit

---
+++ src/pages/index.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { metatags } from '@roxi/routify'
+
+    metatags.title = 'Sinar Gold App'
+    metatags.description = ''
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        require('path').join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+    ],
+
+    plugins: [
+        require('@skeletonlabs/skeleton/tailwind/theme.cjs'),
+        require('@tailwindcss/forms'),
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Manrope', ...defaultTheme.fontFamily.sans]
+            }
+        },
+    },
+}

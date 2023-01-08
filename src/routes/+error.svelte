3fdcf655668441cea0a89f00627afa5e041d20a4
risalfajar <risalfajar@gmail.com>
2023-01-08 14:08:59 +0700

    feat: added default error pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import {page} from '$app/stores'
+    import {goto} from "$app/navigation"
+</script>
+
+<div class="w-screen h-screen flex flex-col justify-center items-center">
+    <h2>Maaf, Terjadi Kesalahan</h2>
+    <p>Pesan kesalahan:</p>
+    <pre class="my-4">{$page.status} {$page.error.message}</pre>
+    <button class="mt-8 btn btn-filled-primary" on:click={() => goto('/')}>Kembali</button>
+</div>

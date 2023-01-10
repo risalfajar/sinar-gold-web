b9650028de229be335f41e6fdc8787c10aa100f9
risalfajar <risalfajar@gmail.com>
2023-01-10 23:24:28 +0700

    fix: can't press Enter to save/submit

--- src/lib/common/ui/dialog/FormDialog.svelte
+++ src/lib/common/ui/dialog/FormDialog.svelte
@@ -12,12 +12,15 @@
 <!-- -mx-1 is needed so we can apply px-1 to the form, because showing scrollbar without adding padding clipped the form content -->
 <div class="flex flex-col max-h-[75vh] w-full -mx-1">
     <hr class="opacity-50"/>
-    <form class="min-w-fit px-1 py-4 space-y-4 overflow-y-auto" on:submit|preventDefault={() => isFormValid && dispatch('save')}>
-        <slot></slot>
+    <form class="min-w-fit flex flex-col overflow-y-hidden" on:submit|preventDefault={() => isFormValid && dispatch('save')}>
+        <div class="flex flex-col gap-4 px-1 py-4 overflow-y-auto">
+            <slot></slot>
+        </div>
+        <div class="flex flex-row px-1 justify-end gap-2 mt-4">
+            <Button type="button" on:click={() => isSaving || modalStore.close()}>Kembali</Button>
+            <Button type="submit" isLoading={isSaving} disabled={!isFormValid} class="btn-filled-secondary">
+                Simpan
+            </Button>
+        </div>
     </form>
-    <div class="flex flex-row justify-end gap-2 mt-4 mx-1">
-        <Button on:click={() => isSaving || modalStore.close()}>Kembali</Button>
-        <Button isLoading={isSaving} on:click={() => dispatch('save')} disabled={!isFormValid} type="submit" class="btn-filled-secondary">Simpan
-        </Button>
-    </div>
 </div>

0030a656205de9ce79319d53b46591cdcfc2e845
risalfajar <risalfajar@gmail.com>
2023-01-10 09:41:05 +0700

    feat: add class prop

--- src/lib/common/ui/container/ContainerCard.svelte
+++ src/lib/common/ui/container/ContainerCard.svelte
@@ -2,7 +2,7 @@
     export let title: string
 </script>
 
-<div class="flex flex-col flex-grow w-full bg-white shadow-sm p-8 rounded-2xl space-y-4">
+<div class="flex flex-col flex-grow w-full bg-white shadow-sm p-8 rounded-2xl space-y-4 {$$props.class}">
     <p class="unstyled text-secondary-500 font-bold mb-4">{title}</p>
     <slot/>
 </div>

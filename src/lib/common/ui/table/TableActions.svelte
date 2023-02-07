d9fc45bd092a1440f025a979b3f2623958f3a5c0
risalfajar <risalfajar@gmail.com>
2023-02-07 18:16:54 +0700

    fix: items wrapped

--- src/lib/common/ui/table/TableActions.svelte
+++ src/lib/common/ui/table/TableActions.svelte
@@ -7,7 +7,7 @@
     export let disableDeleteButton = false
 </script>
 
-<div class="flex-none flex-row space-x-2">
+<div class="flex flex-row space-x-2">
     <slot name="prefix"/>
     <IconButton class="border" on:click={(e) => {e.stopPropagation(); dispatch('edit')}}>
         <i class="material-icons text-secondary-500">edit</i>

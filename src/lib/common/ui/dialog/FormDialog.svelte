489b1b5b1f6eb3961e59ef20fd78444138ba2470
risalfajar <risalfajar@gmail.com>
2023-02-23 12:29:52 +0700

    feat: add divider and buttons options

--- src/lib/common/ui/dialog/FormDialog.svelte
+++ src/lib/common/ui/dialog/FormDialog.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
-    import Button from "$lib/common/ui/button/Button.svelte"
-    import {createEventDispatcher} from "svelte"
-    import {closeModal} from "$lib/common/utils/modalUtils"
+	import Button from "$lib/common/ui/button/Button.svelte"
+	import {createEventDispatcher} from "svelte"
+	import {closeModal} from "$lib/common/utils/modalUtils"
 
-    export let isFormValid: boolean = true
-    export let isSaving: boolean = false
+	export let isFormValid: boolean = true
+	export let isSaving: boolean = false
+	export let showDivider: boolean = true
 
-    const dispatch = createEventDispatcher()
+	const dispatch = createEventDispatcher()
 </script>
 
 <!-- -mx-1 is needed so we can apply px-1 to the form, because showing scrollbar without adding padding clipped the form content -->
 <div class="flex flex-col max-h-[75vh] w-full -mx-1">
-    <hr/>
+    {#if showDivider}
+        <hr/>
+    {/if}
     <form class="min-w-fit flex flex-col overflow-y-hidden" on:submit|preventDefault={() => isFormValid && dispatch('save')}>
         <div class="flex flex-col gap-4 px-1 overflow-y-auto">
             <slot></slot>
         </div>
         <div class="flex flex-row px-1 justify-end gap-2 mt-4">
-            <Button type="button" on:click={() => isSaving || closeModal()}>Kembali</Button>
-            <Button type="submit" isLoading={isSaving} disabled={!isFormValid} class="variant-filled-secondary">
-                Simpan
-            </Button>
+            <slot name="buttons">
+                <Button type="button" on:click={() => isSaving || closeModal()}>Kembali</Button>
+                <Button type="submit" isLoading={isSaving} disabled={!isFormValid} class="variant-filled-secondary">
+                    Simpan
+                </Button>
+            </slot>
         </div>
     </form>
 </div>

1abfa22e892b64dc166eda3ed3cbc07a9981fcf1
risalfajar <risalfajar@gmail.com>
2023-03-05 23:18:07 +0700

    feat: FormDialog.svelte

---
+++ src/lib/common/ui/dialog/FormDialog.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ModalTitle from "$lib/common/ui/dialog/ModalTitle.svelte"
+	import FormDialogContent from "$lib/common/ui/dialog/FormDialogContent.svelte"
+
+	export let title: string = ''
+	export let showCloseButton: boolean = false
+	export let isFormValid: boolean = true
+	export let isSaving: boolean = false
+	export let showDivider: boolean = false
+</script>
+
+<div class="space-y-2 {$$props.class}">
+    <ModalTitle {title} {showCloseButton} on:close/>
+    <FormDialogContent {isFormValid} {isSaving} {showDivider} on:save>
+        <slot/>
+    </FormDialogContent>
+</div>

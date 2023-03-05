ddd39a5bbf8dcd7770baaf95470d974d595c69a4
risalfajar <risalfajar@gmail.com>
2023-03-05 23:18:27 +0700

    feat: TextBoxInput

---
+++ src/lib/common/ui/form/TextBoxInput.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import InputWrapper from "$lib/common/ui/form/InputWrapper.svelte"
+
+	export let value
+	export let label: string = ''
+	export let helper: string = ''
+	export let hint: string = 'Masukkan ' + label
+	export let dark: boolean = false
+	export let required: boolean = false
+</script>
+
+<InputWrapper let:classes {label} {dark} {helper} {required}>
+    <textarea
+            placeholder={hint}
+            bind:value
+            on:input
+            on:keydown
+            {...$$restProps}
+            {required}
+            class="input {classes}"></textarea>
+</InputWrapper>

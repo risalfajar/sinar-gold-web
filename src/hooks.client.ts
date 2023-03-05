dff362f0a3b6fa30135f8727da77f2bd9a66882a
risalfajar <risalfajar@gmail.com>
2023-03-05 18:03:22 +0700

    feat: reduce traceSampleRate

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -8,7 +8,7 @@ if (isProd)
 	SentrySvelte.init({
 		dsn: 'https://5d4c36dd4693484a87c0a21e3541f1cc@o4504777970876416.ingest.sentry.io/4504777975267328',
 		integrations: [new BrowserTracing()],
-		tracesSampleRate: 1.0,
+		tracesSampleRate: 0.1,
 	})
 
 // This will catch errors in load functions from +page.ts files

28a43fced7df93d5b4922d4d0a05a94c2ac16979
risalfajar <risalfajar@gmail.com>
2023-03-08 15:25:32 +0700

    feat: radio buttons

---
+++ src/lib/common/ui/form/RadioButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import {getContext} from "svelte"
+
+	export let label: string
+	export let value: any
+	export let group: string = ''
+	export let checked: boolean = false
+	export let name: string = getContext('radio-name')
+</script>
+
+<label class="flex items-center space-x-2">
+    <input class="radio transition-all" type="radio" {checked} {name} {value} bind:group/>
+    <p>{label}</p>
+</label>
---
+++ src/lib/common/ui/form/RadioButtonGroup.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Label from "$lib/common/ui/form/Label.svelte"
+	import {setContext} from "svelte"
+
+	export let label: string
+	export let name: string
+
+	$: setContext('radio-name', name)
+</script>
+
+<div class="flex flex-col">
+    <Label>{label}</Label>
+    <div class="flex flex-grow items-center gap-2">
+        <slot/>
+    </div>
+</div>

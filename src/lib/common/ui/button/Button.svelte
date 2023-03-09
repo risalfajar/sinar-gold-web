3c840e420660403a32c5ebf68f829c6e4b8a5e5a
risalfajar <risalfajar@gmail.com>
2023-03-09 23:42:57 +0700

    feat: button type

--- src/lib/common/ui/button/Button.svelte
+++ src/lib/common/ui/button/Button.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-    import {ProgressRadial} from "@skeletonlabs/skeleton"
-    import {createEventDispatcher} from "svelte"
+	import {ProgressRadial} from "@skeletonlabs/skeleton"
+	import {createEventDispatcher} from "svelte"
 
-    const dispatch = createEventDispatcher()
+	const dispatch = createEventDispatcher()
 
-    export let disabled: boolean = false
-    export let isLoading: boolean = false
+	export let type: 'submit' | 'button' | 'reset' = 'submit'
+	export let disabled: boolean = false
+	export let isLoading: boolean = false
 
-    $: defaultClass = $$props?.class?.includes('variant-') ? '' : 'hoverable'
+	$: defaultClass = $$props?.class?.includes('variant-') ? '' : 'hoverable'
 </script>
 
-<button {...$$restProps} class="btn font-bold {defaultClass} {$$props.class}" {disabled} on:click={() => isLoading || dispatch('click')}>
+<button {...$$restProps} {type} class="btn font-bold {defaultClass} {$$props.class}" {disabled} on:click={() => isLoading || dispatch('click')}>
     {#if isLoading}
         <ProgressRadial class="w-4 mr-2 !opacity-100"/>
     {/if}

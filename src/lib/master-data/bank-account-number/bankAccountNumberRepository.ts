17d9c5c8efd27817c6fd5ea32fe11601b5eabfcf
risalfajar <risalfajar@gmail.com>
2023-03-11 23:52:17 +0700

    ref: change visibility

--- src/lib/master-data/bank-account-number/bankAccountNumberRepository.ts
+++ src/lib/master-data/bank-account-number/bankAccountNumberRepository.ts
@@ -5,7 +5,7 @@ import {NODE_BANK_ACCOUNT_NUMBER, NODE_MASTER_DATA} from "$lib/constants"
 import {BankAccountNumber} from "$lib/master-data/bank-account-number/bankAccountNumber"
 
 export default class BankAccountNumberRepository extends MapDatabaseRepository<BankAccountNumber> {
-    protected primaryKey: keyof BankAccountNumber = "id"
+    primaryKey: keyof BankAccountNumber = "id"
 
     getDocumentReference(): DatabaseReference {
         return ref(Database, `${NODE_MASTER_DATA}/${NODE_BANK_ACCOUNT_NUMBER}`)
